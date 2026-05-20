5796d80a4cabf4bfdf1512fb2ade5aabcfe2ff82
mjw012405170509-max <mjw012405170509@gmail.com>
2026-05-20 12:44:21 +0900

    .gitignore설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,12 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
       <h1 className="title">ESG 프로젝트 화이팅</h1>
-    </>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
   );
 }
 
